7a973d83eddbc879d285ff566903af4d70c4316e
riccardodm97 <riccardo.dm97@gmail.com>
2020-04-23 19:21:48 +0200

    prova di setup
    calendar prova

--- App.js
+++ App.js
@@ -6,7 +6,15 @@ import { Calendar } from 'react-native-calendars';
 const App = () => {
   return(
     <View>
-      <Calendar />
+      <Calendar
+  // Collection of dates that have to be marked. Default = {}
+  markedDates={{
+    '2020-04-16': {selected: true, marked: true, selectedColor: 'blue'},
+    '2020-04-17': {marked: true},
+    '2020-04-18': {marked: true, dotColor: 'red', activeOpacity: 0},
+    '2020-04-19': {disabled: true, disableTouchEvent: true}
+  }}
+/>
     </View>
   );
 }
