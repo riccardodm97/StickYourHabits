dc5cd23d1aee273c7c3c218992c96f5f2f6723cf
Wild0rez <wild0rez@MacBook-Pro.local>
2020-04-25 15:55:34 +0200

    Ripristinata la pagina App.js

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Calendar } from 'react-native-calendars';
-
+import {View, Text} from 'react-native';
 
 const App = () => {
-  return(
+  return (
     <View>
-      <Calendar
-  // Collection of dates that have to be marked. Default = {}
-  markedDates={{
-    '2020-04-16': {selected: true, marked: true, selectedColor: 'blue'},
-    '2020-04-17': {marked: true},
-    '2020-04-18': {marked: true, dotColor: 'red', activeOpacity: 0},
-    '2020-04-19': {disabled: true, disableTouchEvent: true}
-  }}
-/>
+      <Text />
     </View>
   );
-}
+};
 
 export default App;
