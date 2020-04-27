d021bab51fec57ebca2b8a9bd00b02219dec6fcd
riccardodm97 <riccardo.dm97@gmail.com>
2020-04-27 14:46:47 +0200

    refactor organizzazione app
    fattorizzate componenti da condividere nell'app
    aggiunte cartelle che non so se vedrai dopo il push perchè sono ancora vuote

--- App.js
+++ App.js
@@ -1,74 +1,12 @@
-import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icons from 'react-native-vector-icons/dist/FontAwesome';
-import Calendar from './src/scenes/CalendarPage/index';
-import Settings from './src/scenes/SettingsPage/index';
-import Prog  from './src/scenes/ProgPage/index';
-import Habits  from './src/scenes/HabitsPage/index';
+import BottomNavBar from './src/components/bottomNavBar'
 
 //SE LA PORCO DUE DI APPLICAZIONE NON PARTE : cd android -> gradlew --refresh-dependencies
 
 
-// In attesa di trovare soluzione va tenuto.
-// Questo serve per far si che su iOS venga riconosciuto FontAwesome
-
- Icons.loadFont()
-
-
-const Tab = createBottomTabNavigator();
-
-
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName='Calendario'
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Progressi') {
-              iconName = 'bar-chart'
-            } else if (route.name === 'Calendario') {
-              iconName = 'calendar'
-            } else if (route.name === 'Abitudini'){
-              iconName = 'check-circle'
-            }else if(route.name === 'Impostazioni'){
-              iconName = 'cog'
-            }
-
-            // You can return any component that you like here!
-            return <Icons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: '#49a6c8',
-          inactiveTintColor: 'gray',
-        }}
-      >
-
-        <Tab.Screen
-          name="Progressi"
-          component={Prog}
-        />
-        <Tab.Screen
-          name="Calendario"
-          component={Calendar}
-
-        />
-        <Tab.Screen
-          name="Abitudini"
-          component={Habits}
-        />
-        <Tab.Screen
-          name="Impostazioni"
-          component={Settings}
-        />
-
-      </Tab.Navigator>
-    </NavigationContainer>
+      <BottomNavBar initialRouteName={'Calendario'}/>   
   );
 }
 
