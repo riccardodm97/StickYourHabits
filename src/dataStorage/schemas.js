3f38b76b68235f8d7721e59357fa29a5a3c6d7ec
wild0rez <mau.dirollo@stud.uniroma3.it>
2020-04-29 09:37:08 +0200

    Aggiunto un altro esempio tra le abitudini per controllare il posizionamento e installate dipendenze

--- src/dataStorage/schemas.js
+++ src/dataStorage/schemas.js
@@ -1,23 +1,24 @@
-import Realm from 'realm'
+import Realm from 'realm';
 
 export const HABITS_SCHEMA = 'HabitSchema';
 
 export const HabitSchema = {
-    name: HABITS_SCHEMA,
-    primaryKey: 'id',
-    properties : {
-        id: 'int',
-        name : 'string',
-        low : 'string',
-        medium : 'string',
-        high : 'string'
-    }
+  name: HABITS_SCHEMA,
+  primaryKey: 'id',
+  properties: {
+    id: 'int',
+    name: 'string',
+    low: 'string',
+    medium: 'string',
+    high: 'string',
+  },
 };
 
 const databaseOptions = {
-    path : Realm.defaultPath,
-    schema : [HabitSchema],
-    schemaVersion : 0,            //optional , ci serve? 
-}
+  path: Realm.defaultPath,
+  schema: [HabitSchema],
+  schemaVersion: 0, //optional , ci serve?
+};
 
-export default new Realm(databaseOptions); 
+export default new Realm(databaseOptions);
+
