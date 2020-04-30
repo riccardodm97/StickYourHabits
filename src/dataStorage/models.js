87818088245af0bf84f70e6bfae23e2a134f8ae5
riccardodm97 <riccardo.dm97@gmail.com>
2020-04-30 14:06:11 +0200

    quasi finita la gestione delle abitudini
    fix habitservice delete
    finita pagina creahabit
    creato model per habit
    rename generali

---
+++ src/dataStorage/models.js
@@ -0,0 +1,10 @@
+
+export class Habit {
+    constructor(id, name, low, medium, high) {
+        this.id = id;
+        this.name = name;
+        this.low = low;
+        this.medium = medium;
+        this.high = high;
+    }
+}
