7af7d657e9172b3eab45e39b07e7171840f2427b
Wild0rez <wild0rez@MacBook-Pro.local>
2020-05-02 14:23:33 +0200

    Aggiunt props onPress per il dayComponent.

--- src/scenes/CalendarPage/index.js
+++ src/scenes/CalendarPage/index.js
@@ -36,7 +36,7 @@ const Calendar = ({ navigation }) => {
                 <Text style={styles.monthNameText}> {month} </Text>
                 <View style={styles.calendar}>
                     {currentMonthDaysArray.map((day) => {
-                        return <DayComponent key={day.id} text={day.value} />
+                        return <DayComponent key={day.id} text={day.value}  onPress={() => console.log(day.id)}/>
                     })}
                 </View>
                 <View style={styles.buttons}>
