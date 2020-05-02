cc909a1cdb4e0dc1ee65e2206a2dec6b848806fb
Wild0rez <wild0rez@MacBook-Pro.local>
2020-05-02 12:09:03 +0200

    Iniziato a lavorare sul calendario

--- src/scenes/CalendarPage/DayComponent.js
+++ src/scenes/CalendarPage/DayComponent.js
@@ -10,12 +10,24 @@ import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 const DayComponent = props => {
   return (
-    <View>
+    <View style={styles.dayButton}>
       <TouchableOpacity>
-        <Text> Ciao </Text>
+        <Text> {props.text} </Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  dayButton: {
+    height: 50,
+    width: 50,
+    borderRadius: 100,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 8 
+  }
+})
 
 export default DayComponent;
