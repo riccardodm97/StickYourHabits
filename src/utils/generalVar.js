00504f66a882a9417bdc2c623a4401e8062869f1
wild0rez <mau.dirollo@stud.uniroma3.it>
2020-05-02 14:02:04 +0200

    Aggiunta la localizzazione italiana per i nomi dei mesi. Ora se la pagina viene refereshata torna al mese corrente

--- src/utils/generalVar.js
+++ src/utils/generalVar.js
@@ -8,5 +8,18 @@ export const MAX_HABITS = 3;
 
 export const INITIAL_ROUTE_NAME = 'Calendario'; 
 
-
+export const ITALIAN_NAMES = {
+    0: 'Gennaio',
+    1: 'Febbraio',
+    2: 'Marzo',
+    3: 'Aprile',
+    4: 'Maggio',
+    5: 'Giugno',
+    6: 'Luglio',
+    7: 'Agosto',
+    8: 'Settembre',
+    9: 'Ottobre',
+    10: 'Novembre',
+    11: 'Dicembre',
+}
 
