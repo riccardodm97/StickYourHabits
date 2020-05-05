b66f34c572ac141d6c468684ba710969d904aabb
Wild0rez <wild0rez@MacBook-Pro.local>
2020-05-05 13:26:40 +0200

    Cambiata la libreria per la gestione delle date. Riscritti alcuni metodi e test.

--- src/scenes/CalendarPage/dateHandler.js
+++ src/scenes/CalendarPage/dateHandler.js
@@ -1,62 +1,29 @@
-import XDate from 'xdate';
-import {TODAY} from '../../utils/generalVar'; 
-/*
- * FUNZIONI
- * Ho un oggetto che rappresenta la data corrente e uso quello come punto di partenza.
- * Mi serve:
- * Sapere quanti giorni ha il mese corrente;
- * Il nome del mese corrente
- * Devo poter conoscere anche l'anno ma non lo devo visualizzare
- *
- * Se l'utente va al mese successivo devo sapere il numero di giorni del mese successivo
- * stessa cosa precedente
- */
+import moment from 'moment';
+import {TODAY} from '../../utils/generalVar';
 
-const today = TODAY
+const today = TODAY;
 
-export function getNumberOfDays(date) {
-  return XDate.getDaysInMonth(date.getFullYear(), date.getMonth());
+function monthAndYearToString(dateObj){
+  return dateObj.year + "-" + dateObj.month;
 }
 
-export function getCurrentMonth(date) {
-  return date.getMonth();
-}
-
-// Queste due funzioni permettono di mantenere today invariato creando
-// una copia dell'oggetto e lavorando su quella.
-export function nextMonthCloning() {
-  let nMonth = today.clone();
-  return nMonth.addMonths(1, true);
-}
-
-export function prevMonthCloning() {
-  let pMonth = today.clone();
-  return pMonth.addMonths(-1, true);
-}
-
-export function getCurrentMonthName(date) {
-  return date.toString('MMMM');
-}
-
-// Funzioni che consentono di variare today per la sessione corrente
-export function nextMonth(date) {
-  return date.addMonths(1, true);
-}
-
-export function prevMonth(date) {
-  return date.addMonths(-1, true);
-}
-
-export function getDayID(date) {
-  return date.toString('yyyyMM');
-}
-
-export function dayArray(date) {
-  const days = [];
-  for(let i = 1; i <= getNumberOfDays(date); i++) {
-    const day = {id: (getDayID(date).concat(i.toString())), value: i}
-    days.push(day);
+export function getCurrentMonthAndYear(){
+  return { 
+    month: today.format('M'),
+    year : today.format('YYYY'),
   }
-  
-  return days;
+}
+
+export function getNumberOfDays(dateObj) {
+  const date = monthAndYearToString(dateObj);
+  return moment(date, "YYYY-MM").daysInMonth();
+}
+
+export function nextMonth(dateObj) {
+  const currentDate = moment(monthAndYearToString(dateObj), "YYYY-MM");
+  const nextMonth = currentDate.add(1, 'month');
+  return {
+    month: nextMonth.format('M'),
+    year:  nextMonth.format('YYYY'),
+  }
 }
