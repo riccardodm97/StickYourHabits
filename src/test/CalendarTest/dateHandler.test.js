0247710ebca6a1f1c8f99f2bcec25b56db163649
wild0rez <mau.dirollo@stud.uniroma3.it>
2020-05-05 18:27:41 +0200

    Cambiata la logica per la gestione del calendario

--- src/test/CalendarTest/dateHandler.test.js
+++ src/test/CalendarTest/dateHandler.test.js
@@ -4,12 +4,12 @@ import moment from 'moment';
 import {
   getNumberOfDays,
   getCurrentMonthAndYear,
-  nextMonthCloning,
-  prevMonthCloning,
   getCurrentMonthName,
-  nextMonth,
-  prevMonth,
+  getNextMonth,
+  getPrevMonth,
   getDayID,
+  getDaysArray,
+  getDayId,
 } from '../../scenes/CalendarPage/dateHandler';
 
 import {TODAY} from '../../utils/generalVar';
@@ -20,13 +20,41 @@ test('Test: Maggio 31, mi aspetto 31', () => {
   const t = getCurrentMonthAndYear();
   expect(getNumberOfDays(t)).toBe(31);
 });
+
 test('Test: Febbraio 01 2020, mi aspetto 29', () => {
   const febr = { month: 2, year: 2020};
   expect(getNumberOfDays(febr)).toBe(29);
 })
-test('Test: Giugno 05 2020, mi aspetto 05 2020', () => {
-  const t = getCurrentMonthAndYear();
-  expect(nextMonth(t).month).toMatch("6");
-  expect(nextMonth(t).year).toMatch("2020");
 
+test('Test: Giugno 06 2020, mi aspetto 06 2020', () => {
+  const t = getCurrentMonthAndYear();
+  expect(getNextMonth(t).month).toMatch("6");
+  expect(getNextMonth(t).year).toMatch("2020");
+
+})
+
+test('Test: Aprile 04 2020, mi aspetto 04 2020', () => {
+  const t = getCurrentMonthAndYear();
+  expect(getPrevMonth(t).month).toMatch("4");
+  expect(getPrevMonth(t).year).toMatch("2020");
+
+})
+
+test('Test: Febbraio 02 2020, mi aspetto 02 2020', () => {
+  let t = getCurrentMonthAndYear();
+  t = getPrevMonth(t);
+  t = getPrevMonth(t);
+  expect(getPrevMonth(t).month).toMatch("2");
+  expect(getPrevMonth(t).year).toMatch("2020");
+
+})
+
+test('Test: voglio un array lungo 29 (29 giorni)', () => {
+  let t = getCurrentMonthAndYear();
+  t = getPrevMonth(t);
+  t = getPrevMonth(t);
+  t = getPrevMonth(t);
+  const days = getDaysArray(t);
+  expect(days.length).toBe(29);
+  expect(getDayId(t, days[4].number)).toMatch("2020-2-5");
 })
